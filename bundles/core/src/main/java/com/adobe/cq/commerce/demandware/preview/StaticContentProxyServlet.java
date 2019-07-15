341423ff898b3201cc1ddb274ff706dadf5aff99
allwardt <allwardt@adobe.com>
2019-07-15 15:02:03 +0200

    Adaption of StaticContentProxyServlet

--- bundles/core/src/main/java/com/adobe/cq/commerce/demandware/preview/StaticContentProxyServlet.java
+++ bundles/core/src/main/java/com/adobe/cq/commerce/demandware/preview/StaticContentProxyServlet.java
@@ -16,13 +16,12 @@
 
 package com.adobe.cq.commerce.demandware.preview;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
+import com.adobe.cq.commerce.demandware.DemandwareClient;
 import com.adobe.cq.commerce.demandware.DemandwareClientProvider;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
@@ -36,11 +35,15 @@ import org.apache.http.util.EntityUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.request.RequestPathInfo;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.adobe.cq.commerce.demandware.DemandwareClient;
+import javax.servlet.ServletException;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
 
 /**
  * Simple proxy for static content assets (images, js, css) with relative URLs to Demandware.
@@ -48,21 +51,21 @@ import com.adobe.cq.commerce.demandware.DemandwareClient;
 @Component(label = "Demandware Static Content Proxy Servlet", immediate = true)
 @SlingServlet(paths = {"/on/demandware"}, extensions = {"static"}, methods = "GET", generateComponent = false)
 public class StaticContentProxyServlet extends SlingSafeMethodsServlet {
-
+    
     private static final Logger LOG = LoggerFactory.getLogger(StaticContentProxyServlet.class);
-
+    
     @Reference
     private DemandwareClientProvider clientProvider;
-
+    
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,
             IOException {
-
+        
         RequestPathInfo pathInfo = request.getRequestPathInfo();
         LOG.debug("Proxy static content for {}", pathInfo.toString());
-        final String remoteUri = DemandwareClient.DEFAULT_SCHEMA + clientProvider.getDefaultClient().getEndpoint() + pathInfo.getResourcePath() +
+        final String remoteUri = DemandwareClient.DEFAULT_SCHEMA + clientProvider.getDemandwareClientByInstanceId(clientProvider.getInstanceId(getPage(request))) + pathInfo.getResourcePath() +
                 "." + pathInfo.getExtension() + pathInfo.getSuffix();
-
+        
         final CloseableHttpClient httpClient = clientProvider.getDefaultClient().getHttpClient();
         CloseableHttpResponse responseObj = null;
         BufferedOutputStream output = null;
@@ -70,7 +73,7 @@ public class StaticContentProxyServlet extends SlingSafeMethodsServlet {
             final RequestBuilder requestBuilder = RequestBuilder.get();
             requestBuilder.setUri(remoteUri);
             final HttpUriRequest requestObj = requestBuilder.build();
-
+            
             responseObj = httpClient.execute(requestObj);
             final HttpEntity responseObjEntity = responseObj.getEntity();
             if (responseObjEntity != null) {
@@ -85,5 +88,21 @@ public class StaticContentProxyServlet extends SlingSafeMethodsServlet {
             HttpClientUtils.closeQuietly(httpClient);
             IOUtils.closeQuietly(output);
         }
+    }
+    
+    private Page getPage(SlingHttpServletRequest request) {
+        String path = getRequestedPath(request);
+        
+        ResourceResolver resourceResolver = request.getResourceResolver();
+        Resource resource = resourceResolver.resolve(path);
+        
+        PageManager pageManager = resource.getResourceResolver().adaptTo(PageManager.class);
+        return pageManager.getPage(path);
+    }
+    
+    private String getRequestedPath(SlingHttpServletRequest request) {
+        String referer = request.getHeader("Referer");
+        String host = request.getHeader("Host");
+        return StringUtils.substringBetween(referer, host, ".html");
     }
 }
